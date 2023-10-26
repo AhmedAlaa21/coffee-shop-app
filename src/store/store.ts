a7a53fddbae6cd6301ea2c393a5f5984d84e61c5
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2023-10-27 01:19:01 +0200

    finished order history logic

--- src/store/store.ts
+++ src/store/store.ts
@@ -135,6 +135,78 @@ export const useStore = create(
             state.FavoritesList.splice(spliceIndex, 1);
           }),
         ),
+      incrementCartItemQuantity: (id: string, size: string) =>
+        set(
+          produce(state => {
+            for (let i = 0; state.CartList.length; i++) {
+              if (state.CartList[i].id === id) {
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (state.CartList[i].prices[j].size === size) {
+                    state.CartList[i].prices[j].quantity++;
+                    break;
+                  }
+                }
+              }
+            }
+          }),
+        ),
+      decrementCartItemQuantity: (id: string, size: string) =>
+        set(
+          produce(state => {
+            for (let i = 0; state.CartList.length; i++) {
+              if (state.CartList[i].id === id) {
+                for (let j = 0; j < state.CartList[i].prices.length; j++) {
+                  if (state.CartList[i].prices[j].size === size) {
+                    state.CartList[i].prices[j].quantity++;
+                    if (state.CartList[i].prices.length > 1) {
+                      if (state.CartList[i].prices[j].quantity > 1) {
+                        state.CartList[i].prices[j].quantity--;
+                      } else {
+                        state.CartList[i].prices.splice(j, 1);
+                        if (state.CartList[i].prices[j].quantity > 1) {
+                          state.CartList[i].prices[j].quantity--;
+                        } else {
+                          state.CartList.splice(i, 1);
+                        }
+                      }
+                      break;
+                    }
+                  }
+                }
+              }
+            }
+          }),
+        ),
+      addToOrderHistoryListFromCart: () =>
+        set(
+          produce(state => {
+            let temp = state.CartList.reduce(
+              (acc: number, currentValue: any) =>
+                acc + parseFloat(currentValue.ItemPrice),
+            );
+            let currentCartListTotalPrice = temp.toFixed(2).toString();
+            if (state.OrderHistoryList.length > 0) {
+              state.OrderHistoryList.unshift({
+                OrderDate:
+                  new Date().toDateString() +
+                  ' ' +
+                  new Date().toLocaleTimeString(),
+                CartList: state.CartList,
+                CartListPrice: currentCartListTotalPrice,
+              });
+            } else {
+              state.OrderHistoryList.push({
+                OrderDate:
+                  new Date().toDateString() +
+                  ' ' +
+                  new Date().toLocaleTimeString(),
+                CartList: state.CartList,
+                CartListPrice: currentCartListTotalPrice,
+              });
+            }
+            state.CartList = [];
+          }),
+        ),
     }),
     {
       name: 'coffee-app',
